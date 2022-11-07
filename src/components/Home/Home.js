6d2d4ecb2c9b9030ff141a74c214afc27bf77129
sazalislam04 <mdsazalislam04@gmail.com>
2022-11-08 00:01:33 +0600

    added header

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <h1>home page</h1>
-    </div>
-  );
+  return <div>{/* <Banner /> */}</div>;
 };
 
 export default Home;
