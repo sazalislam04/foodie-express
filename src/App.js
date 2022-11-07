16b0f5e863091910928e57207fbc653ea1cb5421
sazalislam04 <mdsazalislam04@gmail.com>
2022-11-07 23:40:45 +0600

    setup route on project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/routes";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className="">
+      <RouterProvider router={router} />
     </div>
   );
 }
