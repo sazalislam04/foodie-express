7b0c4bc3431b397322b228d77d2299e9cd0576d7
sazalislam04 <mdsazalislam04@gmail.com>
2022-11-10 17:46:01 +0600

    updated blog

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -69,7 +69,7 @@ const Blog = () => {
       <div className="dark:bg-gray-800 dark:text-gray-100 py-2">
         <div
           className="container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm dark:bg-gray-900"
-          data-aos="fade-left"
+          data-aos="fade-right"
         >
           <div className="flex items-center justify-between">
             <Link
@@ -185,7 +185,7 @@ const Blog = () => {
       <div className="dark:bg-gray-800 dark:text-gray-100 pt-2 pb-10">
         <div
           className="container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm dark:bg-gray-900"
-          data-aos="fade-left"
+          data-aos="fade-right"
         >
           <div className="flex items-center justify-between">
             <Link
